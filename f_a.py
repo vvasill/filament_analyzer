a53be9ed201668fabf5acebfa58bfa5bcf313386
vvasill <pushkarev.vv14@physics.msu.ru>
2018-11-06 23:52:33 +0300

    first

--- f_a.py
+++ f_a.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 
 #setup filenames
 path = sys.argv[1]
-os.chdir( path )
-os.makedirs("weak", exist_ok = True)
-os.makedirs("overscaled", exist_ok = True)
+os.chdir(path)
+
+if (os.path.isdir('weak') == False):
+	os.makedirs('weak')
+if (os.path.isdir('overscaled') == False):
+	os.makedirs('overscaled')
 filenames_txt = glob.glob('*_*.txt')
 filenames_png = glob.glob('*_*.png')
 
@@ -19,21 +22,20 @@ MAX_VAL = 1023
 for filename in filenames_txt:
 
 	data = np.loadtxt(filename)
-	print(filename + 'is processing')
+	print(filename + ' is processing')
 	f_name = filename.split(".txt")[0]
-	f_params = f_name_splitted.split('_')[:]
+	f_params = f_name.split('_')[:]
 
 	x_max, y_max = np.unravel_index(np.argmax(data), data.shape)
 
-	if data[x_max, y_max] > MAX_VAL:
+	if (data[x_max, y_max] > MAX_VAL):
 		os.rename(filename, 'overscaled/' + filename)
 		for f_n in filenames_png:
-			f_name_png_splitted = filename_1.split(".png")[0]
-			f_name_png_params = f_name_png_splitted.split('_')[:]
-			if f_name_png_params[1:3] == f_params[0:2]:
-				os.rename(filename_1, 'overscaled/' + f_n)
+			f_name_png = f_n.split(".png")[0]
+			f_png_params = f_name_png.split('_')[:]
+			if (f_name_png_params[1:3] == f_params[0:2]):
+				os.rename(f_n, 'overscaled/' + f_n)
 				break
-		continue
 	else:
 		s = 0.0
 		for i in range(x_max-1, x_max+2):
@@ -41,12 +43,11 @@ for filename in filenames_txt:
 				if i != x_max or j != y_max:
 					s += data[i,j]
 	
-		if data[x_max, y_max] > s/4.0:
+		if (data[x_max, y_max] > s/4.0):
 			os.rename(filename, 'weak/' + filename)
-			for filename_1 in filenames_png:
-				filename_png_splitted = filename_1.split(".png")[0]
-				f_png_params = filename_png_splitted.split('_')[:]
-				if f_png_params[1:3] == f_params[0:2]:
-					os.rename(filename_1, "weak/"+filename_1)
+			for f_n in filenames_png:
+				f_name_png = f_n.split(".png")[0]
+				f_png_params = f_name_png.split('_')[:]
+				if (f_png_params[1:3] == f_params[0:2]):
+					os.rename(f_n, "weak/" + f_n)
 					break
-			continue
